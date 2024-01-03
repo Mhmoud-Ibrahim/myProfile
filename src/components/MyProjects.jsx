4bcda6e484c857adcf152b9881d176dbdc3408aa
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2024-01-03 20:53:32 +0200

    adfa

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,6 +1,6 @@
 
 import { useEffect} from "react";
-import { Link } from "react-router-dom";
+
 
 import ecomercy from '../assets/e-comercy.jpg'
 import market from '../assets/Market.png'
@@ -24,27 +24,31 @@ import { Helmet } from "react-helmet";
 
 export default function MyProjects() {
  const projectdetails =[
-  {id:1,img:market,name:'Market',link:'https://mhmoud-ibrahim.github.io/market/',discreption:'react website build with real database with api and backend  '},
-  {id:2,img:trending,name:'Trending',link:'https://mhmoud-ibrahim.github.io/trend/',discreption:'react website build with real database with api and backend '},
-  {id:3,img:wather,name:'Weather',link:'https://mhmoud-ibrahim.github.io/Weather/',discreption:'javascript website build to search with api in countries condition this search by name of city '},
-  {id:4,img:yummy,name:'Yummy',link:'https://mhmoud-ibrahim.github.io/Yummyjs/',discreption:'web site build with javascript depend on api , in it we show alot of meals '},
-  {id:5,img:quiz,name:'Quiz',link:'https://mhmoud-ibrahim.github.io/Quiezes/',discreption:' an application with javascript build to learn OOP  there is api  to backend '},
-  {id:6,img:skytouch,name:'Skytouch',link:'https://mhmoud-ibrahim.github.io/skytouch/',discreption:' aportfolio  build in html ,css and javascript this is in the begening of my work'},
-  {id:7,img:games,name:'Games',link:'https://mhmoud-ibrahim.github.io/Games/',discreption:'web site build in javascript depend on api to show alot of different games'},
-  {id:8,img:party,name:'Party',link:'https://mhmoud-ibrahim.github.io/EgyptionParty/',discreption:'an event is egyption party and we wait for it'},
-  {id:9,img:photographer,name:'Photographer',link:'https://mhmoud-ibrahim.github.io/photographer/',discreption:'aportfolio  build in html ,css and javascript this is in the begening of my work'},
-  {id:10,img:meals,name:'Meals',link:'https://mhmoud-ibrahim.github.io/RecipiesFood/',discreption:'web site build with javascript depend on api , in it we show alot of meals  '},
-  {id:11,img:memorygame,name:'Memorygame',link:'https://mhmoud-ibrahim.github.io/Memorygame/',discreption:'memory game that popular game build in html ,css and javascript and sound but it just one level now'},
-  {id:12,img:notes,name:'Notes',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'when i learn to build a CRUDS system to build web site to (Create,Read,Delete and Search )'},
-  {id:13,img:crud,name:'CRUDS',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'Have build CRUD system and learned how to (Create,Read,Update,Delete and search) on the CRUDS aplication with plain javascript' },
-  {id:14,img:Xo,name:'XO',link:'https://mhmoud-ibrahim.github.io/Xo-game/',discreption:'this is a (x.o) the popular gam in react,js'},
-  {id:15,img:quotes,name:'Quotes',link:'https://mhmoud-ibrahim.github.io/Quotes/',discreption:'website that show Quotes build in html,css and javascript '},
-  {id:16,img:ecomercy,name:'E-comercy',link:'https://mhmoud-ibrahim.github.io/E-comercy/',discreption:'react website build with real database with api and backend  '},
+  {id:0,img:market,name:'Market',link:'https://mhmoud-ibrahim.github.io/market/',discreption:'react website build with real database with api and backend  '},
+  {id:1,img:trending,name:'Trending',link:'https://mhmoud-ibrahim.github.io/trend/',discreption:'react website build with real database with api and backend '},
+  {id:2,img:wather,name:'Weather',link:'https://mhmoud-ibrahim.github.io/Weather/',discreption:'javascript website build to search with api in countries condition this search by name of city '},
+  {id:3,img:yummy,name:'Yummy',link:'https://mhmoud-ibrahim.github.io/Yummyjs/',discreption:'web site build with javascript depend on api , in it we show alot of meals '},
+  {id:4,img:quiz,name:'Quiz',link:'https://mhmoud-ibrahim.github.io/Quiezes/',discreption:' an application with javascript build to learn OOP  there is api  to backend '},
+  {id:5,img:skytouch,name:'Skytouch',link:'https://mhmoud-ibrahim.github.io/skytouch/',discreption:' aportfolio  build in html ,css and javascript this is in the begening of my work'},
+  {id:6,img:games,name:'Games',link:'https://mhmoud-ibrahim.github.io/Games/',discreption:'web site build in javascript depend on api to show alot of different games'},
+  {id:7,img:party,name:'Party',link:'https://mhmoud-ibrahim.github.io/EgyptionParty/',discreption:'an event is egyption party and we wait for it'},
+  {id:8,img:photographer,name:'Photographer',link:'https://mhmoud-ibrahim.github.io/photographer/',discreption:'aportfolio  build in html ,css and javascript this is in the begening of my work'},
+  {id:9,img:meals,name:'Meals',link:'https://mhmoud-ibrahim.github.io/RecipiesFood/',discreption:'web site build with javascript depend on api , in it we show alot of meals  '},
+  {id:10,img:memorygame,name:'Memorygame',link:'https://mhmoud-ibrahim.github.io/Memorygame/',discreption:'memory game that popular game build in html ,css and javascript and sound but it just one level now'},
+  {id:11,img:notes,name:'Notes',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'when i learn to build a CRUDS system to build web site to (Create,Read,Delete and Search )'},
+  {id:12,img:crud,name:'CRUDS',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'Have build CRUD system and learned how to (Create,Read,Update,Delete and search) on the CRUDS aplication with plain javascript' },
+  {id:13,img:Xo,name:'XO',link:'https://mhmoud-ibrahim.github.io/Xo-game/',discreption:'this is a (x.o) the popular gam in react,js'},
+  {id:14,img:quotes,name:'Quotes',link:'https://mhmoud-ibrahim.github.io/Quotes/',discreption:'website that show Quotes build in html,css and javascript '},
+  {id:15,img:ecomercy,name:'E-comercy',link:'https://mhmoud-ibrahim.github.io/E-comercy/',discreption:'react website build with real database with api and backend  '},
 ]
 useEffect(()=>{
  AOS.init({duration:2000})
 },[])
-
+ function getlink(index){
+  for(let i=0;projectdetails.length;i++){
+    return window.location.href=projectdetails[index].link
+  }
+ }
 
   return <>
   <Helmet>
@@ -61,7 +65,7 @@ useEffect(()=>{
     <div className="d-flex justify-content-around">
        <h2  className="text-thierd  fs-5 fw-bold">{project?.name}</h2>
     
-    <Link className="btn btn-sm btn-second "  to={project?.link} >show</Link>
+    <button className="btn btn-sm btn-second "  onClick={()=>getlink(project.id)}>show</button>
     </div>
     <p  className="text-light">{project?.discreption}</p>
   </div>
--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -25,22 +25,22 @@ import { Helmet } from "react-helmet";
 export default function Picture() {
 
   const projectdetails =[
-    {id:1,img:market,name:'Market',link:'https://mhmoud-ibrahim.github.io/market/',discreption:'react website build with real database with api and backend  '},
-    {id:2,img:trending,name:'Trending',link:'https://mhmoud-ibrahim.github.io/trend/',discreption:'react website build with real database with api and backend '},
-    {id:3,img:wather,name:'Weather',link:'https://mhmoud-ibrahim.github.io/Weather/',discreption:'javascript website build to search with api in countries condition this search by name of city '},
-    {id:4,img:yummy,name:'Yummy',link:'https://mhmoud-ibrahim.github.io/Yummyjs/',discreption:'web site build with javascript depend on api , in it we show alot of meals '},
-    {id:5,img:quiz,name:'Quiz',link:'https://mhmoud-ibrahim.github.io/Quiezes/',discreption:' an application with javascript build to learn OOP  there is api  to backend '},
-    {id:6,img:skytouch,name:'Skytouch',link:'https://mhmoud-ibrahim.github.io/skytouch/',discreption:' aportfolio  build in html ,css and javascript this is in the begening of my work'},
-    {id:7,img:games,name:'Games',link:'https://mhmoud-ibrahim.github.io/Games/',discreption:'web site build in javascript depend on api to show alot of different games'},
-    {id:8,img:party,name:'Party',link:'https://mhmoud-ibrahim.github.io/EgyptionParty/',discreption:'an event is egyption party and we wait for it'},
-    {id:9,img:photographer,name:'Photographer',link:'https://mhmoud-ibrahim.github.io/photographer/',discreption:'aportfolio  build in html ,css and javascript this is in the begening of my work'},
-    {id:10,img:meals,name:'Meals',link:'https://mhmoud-ibrahim.github.io/RecipiesFood/',discreption:'web site build with javascript depend on api , in it we show alot of meals  '},
-    {id:11,img:memorygame,name:'Memorygame',link:'https://mhmoud-ibrahim.github.io/Memorygame/',discreption:'memory game that popular game build in html ,css and javascript and sound but it just one level now'},
-    {id:12,img:notes,name:'Notes',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'when i learn to build a CRUDS system to build web site to (Create,Read,Delete and Search )'},
-    {id:13,img:crud,name:'CRUDS',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'Have build CRUD system and learned how to (Create,Read,Update,Delete and search) on the CRUDS aplication with plain javascript' },
-    {id:14,img:Xo,name:'XO',link:'https://mhmoud-ibrahim.github.io/Xo-game/',discreption:'this is a (x.o) the popular gam in react,js'},
-    {id:15,img:quotes,name:'Quotes',link:'https://mhmoud-ibrahim.github.io/Quotes/',discreption:'website that show Quotes build in html,css and javascript '},
-    {id:16,img:ecomercy,name:'E-comercy',link:'https://mhmoud-ibrahim.github.io/E-comercy/',discreption:'react website build with real database with api and backend  '},
+    {id:0,img:market,name:'Market',link:'https://mhmoud-ibrahim.github.io/market/',discreption:'react website build with real database with api and backend  '},
+    {id:1,img:trending,name:'Trending',link:'https://mhmoud-ibrahim.github.io/trend/',discreption:'react website build with real database with api and backend '},
+    {id:2,img:wather,name:'Weather',link:'https://mhmoud-ibrahim.github.io/Weather/',discreption:'javascript website build to search with api in countries condition this search by name of city '},
+    {id:3,img:yummy,name:'Yummy',link:'https://mhmoud-ibrahim.github.io/Yummyjs/',discreption:'web site build with javascript depend on api , in it we show alot of meals '},
+    {id:4,img:quiz,name:'Quiz',link:'https://mhmoud-ibrahim.github.io/Quiezes/',discreption:' an application with javascript build to learn OOP  there is api  to backend '},
+    {id:5,img:skytouch,name:'Skytouch',link:'https://mhmoud-ibrahim.github.io/skytouch/',discreption:' aportfolio  build in html ,css and javascript this is in the begening of my work'},
+    {id:6,img:games,name:'Games',link:'https://mhmoud-ibrahim.github.io/Games/',discreption:'web site build in javascript depend on api to show alot of different games'},
+    {id:7,img:party,name:'Party',link:'https://mhmoud-ibrahim.github.io/EgyptionParty/',discreption:'an event is egyption party and we wait for it'},
+    {id:8,img:photographer,name:'Photographer',link:'https://mhmoud-ibrahim.github.io/photographer/',discreption:'aportfolio  build in html ,css and javascript this is in the begening of my work'},
+    {id:9,img:meals,name:'Meals',link:'https://mhmoud-ibrahim.github.io/RecipiesFood/',discreption:'web site build with javascript depend on api , in it we show alot of meals  '},
+    {id:10,img:memorygame,name:'Memorygame',link:'https://mhmoud-ibrahim.github.io/Memorygame/',discreption:'memory game that popular game build in html ,css and javascript and sound but it just one level now'},
+    {id:11,img:notes,name:'Notes',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'when i learn to build a CRUDS system to build web site to (Create,Read,Delete and Search )'},
+    {id:12,img:crud,name:'CRUDS',link:'https://mhmoud-ibrahim.github.io/cruds1/',discreption:'Have build CRUD system and learned how to (Create,Read,Update,Delete and search) on the CRUDS aplication with plain javascript' },
+    {id:13,img:Xo,name:'XO',link:'https://mhmoud-ibrahim.github.io/Xo-game/',discreption:'this is a (x.o) the popular gam in react,js'},
+    {id:14,img:quotes,name:'Quotes',link:'https://mhmoud-ibrahim.github.io/Quotes/',discreption:'website that show Quotes build in html,css and javascript '},
+    {id:15,img:ecomercy,name:'E-comercy',link:'https://mhmoud-ibrahim.github.io/E-comercy/',discreption:'react website build with real database with api and backend  '},
   ]
   const settings = {
     dots: true,
@@ -50,7 +50,11 @@ export default function Picture() {
     slidesToScroll: 1,
     autoplay:true
   };
-
+  function getlink(index){
+    for(let i=0;projectdetails.length;i++){
+      return window.location.href=projectdetails[index].link
+    }
+   }
 
   useEffect(()=>{
     AOS.init({duration:2000})
@@ -71,11 +75,11 @@ export default function Picture() {
 <div  className="row mt-4">
   {projectdetails.map((project,index)=><>
    <div key={index}  className="col-md-4">
-    <div  className="projectCard    cursor-pointer  text-center p-2">
-      <Link to={project?.link}>
+    <div onClick={()=>getlink(project.id)}  className="projectCard    cursor-pointer  text-center p-2">
+    
       <img data-aos="fade-right"    data-aos-duration={'1500'+project.id} src={project.img}  className="w-100 border border-1 border-light img-fluid" loading="lazy" alt="" />
       <h5 className="text-main">{project.name}</h5>
-      </Link>
+     
 
     </div>
    </div>
