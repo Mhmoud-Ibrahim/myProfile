b4bc97ae97b107c575625ee31687731676441db6
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2024-01-03 19:43:37 +0200

    fdfd

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ import MyProjects from './components/MyProjects'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
 import Picture from './components/Picture'
-import ProtectedRoute from './components/ProtectedRoute'
+
 
 
 function App() {
  const routers =createBrowserRouter([
   {path:'',element:<Layout/>,children:[
-    {index:true,element: <ProtectedRoute><Home/></ProtectedRoute> },
-    {path:'about',element: <ProtectedRoute><About/></ProtectedRoute> },
-    {path:'myProjects',element:<ProtectedRoute><MyProjects/></ProtectedRoute> },
-    {path:'contact',element: <ProtectedRoute><Contact/></ProtectedRoute>},
-    {path:'picture',element: <ProtectedRoute><Picture/></ProtectedRoute>},
+    {index:true,element: <Home/>},
+    {path:'home',element: <Home/>},
+    {path:'about',element: <About/> },
+    {path:'myProjects',element:<MyProjects/> },
+    {path:'contact',element: <Contact/>},
+    {path:'picture',element: <Picture/>},
     {path:'*',element:<NotFound/>},
   ]}
  ])
--- src/components/ProtectedRoute.jsx
+++
@@ -1,5 +0,0 @@
-
-export default function ProtectedRoute(props) {
-    return props.children;
-    
-}
